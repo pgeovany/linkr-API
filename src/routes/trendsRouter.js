a5c718af8be698038785736c5ca72cdb3cabd91e
Nayara <nrdeandrade@gmail.com>
2022-08-13 22:47:13 -0300

    feat: adiciona validação de usuário nas rotas das trends

--- src/routes/trendsRouter.js
+++ src/routes/trendsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getTrends, getTrendByName } from '../controllers/trendsController.js';
+import tokenMiddleware from '../middlewares/tokenMiddleware.js';
 
 const trendsRouter = Router();
 
-trendsRouter.get('/trending', getTrends);
-trendsRouter.get('/trending/:name', getTrendByName);
+trendsRouter.get('/trending', tokenMiddleware, getTrends);
+trendsRouter.get('/trending/:name', tokenMiddleware, getTrendByName);
 
 export default trendsRouter;
