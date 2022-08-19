2877836cc28147bc493f7ece1c084c4cede3c8cb
Nayara <nrdeandrade@gmail.com>
2022-08-18 23:57:58 -0300

    chore: add schema validator para o comentário

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { postComment, getPostComments } from '../controllers/commentController.js';
+
 import tokenMiddleware from '../middlewares/tokenMiddleware.js';
+import validateSchema from '../middlewares/schemaValidator.js';
+import { commentSchema } from '../utils/schemas.js';
 
 const commentsRouter = Router();
 
-commentsRouter.post('/comments/:postId', tokenMiddleware, postComment);
+commentsRouter.post('/comments/:postId', validateSchema(commentSchema), tokenMiddleware, postComment);
 commentsRouter.get('/comments/:postId', tokenMiddleware, getPostComments);
 
 
